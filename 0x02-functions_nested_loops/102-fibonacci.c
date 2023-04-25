bc207541df9c7d78d2ea08457a621b6f3d970934
Princess-Nnamdi <pnnamdi2020@gmail.com>
2023-04-25 22:54:05 +0000

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the first 52 fibonacci numbers
  * Return: always 0
  * Description: printing Fibonacci numbers
  **/
@@ -14,7 +14,7 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	printf(", %ld", j);
+	printf("%ld", j);
 	else if (i == 1)
 	printf(", %ld", k);
 	else
