cf9d3a1654e97bdf4a7d089ca279b4e9d4947cbd
root <pnnamdi2020@gmail.com>
2023-04-27 14:48:11 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints from 0 to 14 ten times
+ * Return: void
+ * Description: prints severally
+ **/
+
+void more_numbers(void)
+{
+	char i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
+		}
+	_putchar('\n');
+	}
+
+}
