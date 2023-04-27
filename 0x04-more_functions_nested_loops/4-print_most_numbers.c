b4cfcb0323e5d5037f9bb6d07cc32aacf938634c
root <pnnamdi2020@gmail.com>
2023-04-27 09:24:46 -0700

    printing some numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- prints 0 to 9 without 2 and 4
+ * Description: prints some numbers
+ **/
+
+void print_most_numbers(void)
+{
+	int c = 0, d;
+
+	while (c < 8)
+	{
+		d = 0;
+		while (d < 8)
+		{
+			if (d == 2 || d == 4)
+				continue;
+		_putchar(d);
+		d++;
+		}
+	_putchar('\n');
+	c++;
+	}
+}
