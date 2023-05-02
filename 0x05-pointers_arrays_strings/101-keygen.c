071082712ea1f48f7f3ffc72df30fe13224ae037
root <pnnamdi2020@gmail.com>
2023-05-02 15:25:37 -0700

    password key generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,37 +11,25 @@
 
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int i, sum, n;
+	int pass[100];
 
-	srand(time(0));
+	sum = 0;
 
-	while (sum < 2772)
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-
-		if ((sum - 2772) % 2 != 0)
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			diff_half1++;
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
 		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
-
+		return (0);
 	}
-	printf("%s", password);
-	return (0);
 }
