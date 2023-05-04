02810ca5a1435056c32b6ecc8c03e63794769381
root <pnnamdi2020@gmail.com>
2023-05-04 11:41:42 -0700

    comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcmp- a function that compares two strings
+ * Return: 0 on success
+ * Description: a function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && s1 == s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
