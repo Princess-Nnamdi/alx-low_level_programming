e3c7717e630611746cc71366b8b8aae6caec32cc
root <pnnamdi2020@gmail.com>
2023-05-04 07:20:16 -0700

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat- concatenates strings
+ * Return: a pointer to the resulting string dest
+ * Description: concatenates strings
+ * @dest: destination string
+ * @src: source string
+ **/
+
+char *_strcat(char *dest, char *src)
+{
+	int length = 0, i;
+
+	while (dest[length])
+	{
+		length++;
+	}
+
+	for (i = 0; src[i] != '0'; i++)
+	{
+		dest[length] = src[i];
+		length++;
+	}
+
+	dest[length] = '\0';
+	return (dest);
+}
