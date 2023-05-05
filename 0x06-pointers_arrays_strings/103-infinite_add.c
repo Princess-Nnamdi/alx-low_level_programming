15af5a75ed1a1a7259ddb8a9c8e97eb225d23142
root <pnnamdi2020@gmail.com>
2023-05-05 09:17:09 -0700

    correction

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,36 +9,24 @@
  * @r: buffer that the function will use to store the result
  * @size_r: is the buffer size
  **/
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
 	while (*(n1 + c1) != '\0')
-	{
 		c1++;
-	}
 	while (*(n2 + c2) != '\0')
-	{
 		c2++;
-	}
 	if (c1 >= c2)
-	{
 		bg = c1;
-	}
 	else
-	{
 		bg = c2;
-	}
 	if (size_r <= bg + 1)
-	{
 		return (0);
-	}
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
-	{
 		op = dr1 + dr2 + add;
 		if (op >= 10)
 			add = op / 10;
@@ -58,13 +46,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			dr2 = 0;
 
 		bg--, size_r--;
-	}
 	if (*(r) == '0')
-	{
 		return (r + 1);
-	}
 	else
-	{
 		return (r);
-	}
 }
