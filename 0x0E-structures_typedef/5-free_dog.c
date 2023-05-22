2c241128db2989b42448870b044c6ac2027b4bb8
root <pnnamdi2020@gmail.com>
2023-05-22 07:59:40 -0700

    corrections

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog- frees memory allocated for a struct dog
+ * Description: frees memory of dog
+ * @d: struct to free
+ **/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
