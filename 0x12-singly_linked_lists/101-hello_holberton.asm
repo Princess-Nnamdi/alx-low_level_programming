2d456b44090e5e345973d02c941a6b4810341bcd
Princess-Nnamdi <pnnamdi2020@gmail.com>
2023-06-01 19:27:22 -0700

    printing the Holberton message

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .data
+	hello_msg db "Hello, Holberton", 0
+	format db "%s\n", 0
+
+section .text
+	extern printf
+
+
+global main
+main:
+	push rbp		; Preserve the base pointer
+	mov rbp, rsp		; Set the base pointer
+	sub rsp, 8		; Allocate space on the stack
+	mov rdi, format		; First arguement: format string
+	mov rsi, hello_msg	; Second arguement: message string
+	xor eax, eax		; Clear EAX register
+	call printf		; Call printf function
+	add rsp, 8		; Deallocate space from the stack
+	xor eax, eax		; Set return value to 0 (success)
+	leave			; Restore the base pointer and deallocate stack frame
+	ret			; Return from the main function
