601fa95f4e099aea3ce8afbeeb334cd898738fe2
Princess-Nnamdi <pnnamdi2020@gmail.com>
2023-06-13 10:32:39 -0700

    fixing bugs in a file with read and write permissions for superuser

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,16 +12,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int files, writes = 0, len = 0;
+	int files, writes, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	files = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	writes = write(files, text_content, len);
 	if (files == -1 || writes == -1)
 		return (-1);
 
