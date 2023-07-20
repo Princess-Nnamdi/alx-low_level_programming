44e509d1575974004ab6d769c5c3630dbd962d28
Pricess-Nnamdi <pnnamdi2020@gmail.com>
2023-07-20 05:17:42 -0700

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
 /**
  * print_dlistint- prints all elemernts in dlistint_t list
  * @h: pointer to head of link
- * Retrun: number of nodes
+ * Return: number of nodes
  **/
 
 size_t print_dlistint(const dlistint_t *h)
@@ -17,7 +17,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h->prev)
 		h = h->prev;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
